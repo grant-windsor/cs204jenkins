b3a05ec0551d097d75562f9def092492ad09e5a0
Grant Windsor <59716944+grant-windsor@users.noreply.github.com>
2021-10-15 19:10:04 -0600

    Add files via upload

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,89 @@
+
+class Calculator {
+
+    Calculator(){
+
+    }
+
+    int add(int a , int b){
+        return a + b;
+    }
+
+    int subtract(int a , int b){
+        return a - b;
+    }
+
+    int multiply(int a , int b){
+        return a * b;
+    }
+
+    int divide(int a , int b){
+        return a / b;
+    }
+
+
+    /*
+    Returns the n'th number in the fibonacci sequence
+    https://en.wikipedia.org/wiki/Fibonacci_number
+    Example below
+    n = x
+    0 = 0
+    1 = 1
+    2 = 1
+    3 = 2
+    4 = 3
+    5 = 5
+    .
+    .
+    .
+    etc
+     */
+    int fibonacciNumberFinder(int n){
+        int a = 0, b = 1, c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
+
+    /*
+    Returns binary value of the given int number
+    The binary number will be stored as a string
+    if int a = 0 then this method returns: 0
+    if int a = 10 then this method returns: 1010
+    if int a = 16 then this method returns: 10000
+     */
+    String intToBinaryNumber(int n){
+        return Integer.toBinaryString(n);
+    }
+
+    /*
+    Create a completely unique String identifier for a given string
+    Each createdID must contain the string n in its unaltered Form
+    if String n = "Jason"
+    then the created ID could be = Jasonklfgn3jknnvksdfm - Because it contains the unaltered String n and is unique
+
+    if you run this function twice with the same String input, it must return 2 unique String IDs
+     */
+    String createUniqueID(String n){
+
+        String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder builder = new StringBuilder();
+        builder.append(n);
+        int count = 12;
+        while (count-- != 0) {
+            int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+        }
+        return builder.toString();
+
+    }
+
+
+}
